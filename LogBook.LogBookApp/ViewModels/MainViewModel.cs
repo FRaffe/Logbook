14f1b63d2de58b07fb84bd2fea44fd07c306f61b
FRaffe <rkorbes@schueler.hakzell.at>
2024-02-19 12:15:46 +0100

    Add entries collection and load command

--- LogBook.LogBookApp/ViewModels/MainViewModel.cs
+++ LogBook.LogBookApp/ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using LogBook.Lib;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +15,13 @@ public partial class MainViewModel(IRepository repository) : ObservableObject
     public string Header => "Fahrtenbuch";
     IRepository _repository = repository;
 
+    [ObservableProperty]
+    ObservableCollection<Lib.Entry> _entries = [];
 
+
+    [RelayCommand]
+    void LoadData()
+    {
+
+    }
 }
