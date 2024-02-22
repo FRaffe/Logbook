937c1b4ee2f006c89d813ebb81eea7eec5f212c4
FRaffe <rkorbes@schueler.hakzell.at>
2024-02-22 07:49:10 +0100

    GUI

--- LogBook.LogBookApp/ViewModels/MainViewModel.cs
+++ LogBook.LogBookApp/ViewModels/MainViewModel.cs
@@ -22,6 +22,11 @@ public partial class MainViewModel(IRepository repository) : ObservableObject
     [RelayCommand]
     void LoadData()
     {
+        var entries = _repository.GetAll();
 
+        foreach (var entry in entries)
+        {
+            Entries.Add(entry);
+        }
     }
 }
